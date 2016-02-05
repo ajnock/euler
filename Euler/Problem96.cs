9d5be7bd2316cc34328efdfa7c314fc857293bd7
Adam Nock <adam.j.nock@gmail.com>
2016-02-05 00:16:56 -0500

    Problem 96 - Sudoku

---
+++ Euler/Problem96.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    /// <summary>
+    /// Su Doku (Japanese meaning number place) is the name given to a popular puzzle concept. Its origin is unclear, but credit must be attributed to Leonhard Euler who invented a similar, and much more difficult, puzzle idea called Latin Squares. The objective of Su Doku puzzles, however, is to replace the blanks (or zeros) in a 9 by 9 grid in such that each row, column, and 3 by 3 box contains each of the digits 1 to 9. Below is an example of a typical starting puzzle grid and its solution grid.
+    /// A well constructed Su Doku puzzle has a unique solution and can be solved by logic, although it may be necessary to employ "guess and test" methods in order to eliminate options(there is much contested opinion over this). The complexity of the search determines the difficulty of the puzzle; the example above is considered easy because it can be solved by straight forward direct deduction.
+    /// The 6K text file, sudoku.txt (right click and 'Save Link/Target As...'), contains fifty different Su Doku puzzles ranging in difficulty, but all with unique solutions(the first puzzle in the file is the example above).
+    /// By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution grid; for example, 483 is the 3-digit number found in the top left corner of the solution grid above.
+    /// </summary>
+    public class Problem96 : Problem
+    {
+        private IEnumerable<Sudoku> GetBoards()
+        {
+            var games = new List<Sudoku>();
+            IList<string> board = new List<string>();
+            int i = 0;
+            foreach (var line in File.ReadLines("p096_sudoku.txt"))
+            {
+                if (i % 10 != 0)
+                {
+                    board.Add(line);
+                }
+
+                i++;
+
+                if (i % 10 == 0)
+                {
+                    yield return new Sudoku(board);
+                    board = new List<string>();
+                }
+            }
+        }
+
+        int depth = 0;
+        private bool Solve(Sudoku sudoku, out Sudoku solution)
+        {
+            if (sudoku.IsSolved)
+            {
+                solution = sudoku;
+
+                Console.WriteLine();
+                Console.WriteLine(solution);
+                Console.WriteLine();
+
+                return true;
+            }
+
+            foreach (var next in sudoku.Neighbors)
+            {
+                depth++;
+                if (Solve(next, out solution))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(solution);
+                    Console.WriteLine();
+
+                    return true;
+                }
+                depth--;
+            }
+
+            if (depth > maxDepth)
+            {
+                maxDepth = depth;
+                Console.WriteLine(DateTime.Now.ToString() + "\t" + depth);
+            }
+
+            solution = new Sudoku();
+            return false;
+        }
+        int maxDepth = 0;
+        public override object Solve()
+        {
+            var solutions = new List<Sudoku>();
+
+            foreach (var sudoku in GetBoards())
+            {
+                depth = 0;
+                Sudoku solution;
+                if (Solve(sudoku, out solution))
+                {
+                    solutions.Add(solution);
+                }
+            }
+            return string.Join("\r\n", solutions);
+        }
+    }
+}
---
+++ Euler/Sudoku.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Euler
+{
+    public struct Sudoku : IEqualityComparer<Sudoku>, IEquatable<Sudoku>, ICloneable
+    {
+        public int EulerValue
+        {
+            get
+            {
+                return _board[0, 0] + _board[1, 0] + _board[2, 0];
+            }
+        }
+
+        private static readonly HashSet<Sudoku> cache = new HashSet<Sudoku>();
+
+        public bool IsSolved
+        {
+            get
+            {
+                if (!cache.Add(this))
+                {
+                    return false;
+                }
+
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        if (_board[x, y] == 0)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                cache.Remove(this);
+
+                return true;
+            }
+        }
+
+        public bool Set(int x, int y, int value)
+        {
+            if (_board[x, y] == 0 && Test(x, y, value))
+            {
+                _board[x, y] = value;
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerable<Sudoku> Neighbors
+        {
+            get
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        if (_board[x, y] == 0)
+                        {
+                            for (int i = 1; i <= 9; i++)
+                            {
+                                var s = new Sudoku(this);
+                                if (s.Set(x, y, i))
+                                {
+                                    yield return s;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool Test(int x, int y, int value)
+        {
+            var set = new HashSet<int>();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == y)
+                    set.Add(value);
+                else if (_board[x, i] > 0 && !set.Add(_board[x, i]))
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == x)
+                    set.Add(value);
+                else if (_board[i, y] > 0 && !set.Add(_board[i, y]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                foreach (int i in _board)
+                {
+                    if (i > 0)
+                    {
+                        sum++;
+                    }
+                }
+
+                return sum;
+            }
+        }
+        private readonly int[,] _board;
+
+        public Sudoku(IList<string> lines)
+        {
+            if (lines.Count != 9)
+            {
+                throw new ArgumentException();
+            }
+            _board = new int[9, 9];
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    _board[x, y] = int.Parse(lines[y][x].ToString());
+                }
+            }
+        }
+
+        public Sudoku(Sudoku sudoku)
+        {
+            _board = (int[,])sudoku._board.Clone();
+        }
+
+        public bool Equals(Sudoku x, Sudoku y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (x._board[i, j] != y._board[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Sudoku)
+            {
+                return Equals((Sudoku)obj);
+            }
+            return false;
+        }
+
+        public int GetHashCode(Sudoku obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    builder.Append(_board[x, y]);
+                }
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+
+        public bool Equals(Sudoku other)
+        {
+            return Equals(this, other);
+        }
+
+        public object Clone()
+        {
+            return new Sudoku(this);
+        }
+
+        internal bool IsSet(int x, int y)
+        {
+            return _board[x, y] != 0;
+        }
+    }
+
+}
