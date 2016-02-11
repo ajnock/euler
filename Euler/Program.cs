74e5cc5eacfc288ef689607f43944c01e5a04904
Adam Nock <adam.j.nock@gmail.com>
2016-02-10 23:51:57 -0500

    Better UI

--- Euler/Program.cs
+++ Euler/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Euler
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly IDictionary<int, Problem> Problems = new Dictionary<int, Problem>
         {
-            Problem problem = new Problem111();
+            { 10, new Problem10() },
+            { 41, new Problem41() },
+            { 96, new Problem96() },
+            { 49, new Problem49() },
+            { 111, new Problem111() },
+            { 50, new Problem50() }
+        };
+        static int Main(string[] args)
+        {
+            Problem problem;
+            int number;
+
+            if (args.Length < 1)
+            {
+                //Console.WriteLine("Enter a problem number { " + string.Join(", ", Problems.Keys) + " }");
+                //return -1;
+
+                problem = new Problem50();
+            }
+            else if (!int.TryParse(args[0], out number) || !Problems.TryGetValue(number, out problem))
+            {
+                Console.WriteLine("Can't find problem " + args[0]);
+                Console.WriteLine("Enter a problem number { " + string.Join(", ", Problems.Keys) + " }");
+                return 1;
+            }
 
             var timer = new Stopwatch();
             timer.Start();
@@ -15,9 +41,15 @@ namespace Euler
             timer.Stop();
 
             Console.WriteLine();
-            Console.WriteLine("========================================= " + timer.Elapsed.TotalSeconds + "s");
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Solved in " + timer.Elapsed.TotalSeconds + "s");
+            Console.WriteLine("=========================================");
             Console.WriteLine(solution);
-            Console.ReadLine();
+            Console.WriteLine("=========================================");
+
+            Console.WriteLine();
+
+            return 0;
         }
     }
 }
