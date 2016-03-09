8b2ed594aa6c432b2c9446205ee06f508b6bfae8
Adam Nock <adam.j.nock@gmail.com>
2016-03-09 00:53:02 -0500

    Adding Fibonacci and color computations

---
+++ EulerMath/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerMath
+{
+   public class Fibonacci
+    {
+        public static bool IsFibonacci(long n)
+        {
+            long a = 5L * n * n + 4L;
+            long b = 5L * n * n - 4L;
+
+            return IsPerfectSquare(a) || IsPerfectSquare(b);
+        }
+
+        private static bool IsPerfectSquare(long n)
+        {
+            var root =(long) Math.Round(Math.Sqrt(n));
+            return root * root == n;
+        }
+    }
+}
