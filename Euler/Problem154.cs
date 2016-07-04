a8868d9b5feb3a96604f58d1e7dc3959d5db1ed1
Adam Nock <adam.j.nock@gmail.com>
2016-07-04 14:06:52 -0400

    #154

--- Euler/Problem154.cs
+++ Euler/Problem154.cs
@@ -10,54 +10,52 @@ namespace Euler
         public override object Solve()
         {
             const long n = 200000;
-            long max = 0;
-            long product = 1;
-            while (product < 1e12)
-            {
-                max++;
-                product *= max;
-            }
+            //long max = 0;
+            //long product = 1;
+            //while (product < 1e12)
+            //{
+            //    max++;
+            //    product *= max;
+            //}
 
             long total = 0;
-            for (long i = 0; i < 2e5; i++)
+            for (long i = 0; i <= n; i++)
             {
-                for (long j = 0; j < 2e5; j++)
+                long product = 1;
+                if (Factorial(i, ref product))
                 {
-                    for (long k = 0; k < 2e5; k++)
+                    var temp = product;
+                    for (long j = 0; j <= n - i; j++)
                     {
-                        if (i + j + k == n)
+                        long k = n - i - j;
+                        if (i + j + k == n &&
+                            Factorial(j, ref temp) && Factorial(k, ref temp))
                         {
-                            if (TestFactorial(i, j, k))
-                            {
-                                NonBlockingConsole.WriteLine(i + " " + j + " " + k);
-                                total++;
-                            }
+                            NonBlockingConsole.WriteLine(i + " " + j + " " + k);
+                            total++;
                         }
                     }
                 }
             }
 
-            return max;
+            return total;
         }
 
-        private bool TestFactorial(long i, long j, long k)
+        private bool Factorial(long i, ref long product)
         {
             const long max = 1000000000000;
-            long product = 1;
-            while (product < max && i > 0)
+
+            while (product < max && product > 0 && i > 1)
             {
+                try
+                {
+                    product = checked(product * i);
+                }
+                catch (OverflowException)
+                {
+                    return false;
+                }
                 i--;
-                product *= i;
-            }
-            while (product < max && j > 0)
-            {
-                j--;
-                product *= j;
-            }
-            while (product < max && k > 0)
-            {
-                k--;
-                product *= k;
             }
 
             return product < max;
