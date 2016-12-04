cdfa4608234d71260e9cb29f290ad5d648be81d9
Adam Nock <adam.j.nock@gmail.com>
2016-12-04 01:04:45 -0500

    Adding index creation javascripts

---
+++ Ulam/Indexes.js
@@ -0,0 +1,3 @@
+﻿db.map.createIndex({ LongValue: -1 }, { background: true, unique: true });
+db.primes.createIndex({ LongValue: 1 }, { background: true, unique: true });
+db.primes.createIndex({ StringValue: 1 }, { background: true, unique: true });
