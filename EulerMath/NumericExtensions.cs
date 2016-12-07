27945e60f5ce36971af34ee7b72912ce9aadd478
Adam Nock <adam.j.nock@gmail.com>
2016-12-06 23:15:28 -0500

    Batch method.

--- EulerMath/NumericExtensions.cs
+++ EulerMath/NumericExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace EulerMath
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EulerMath
 {
     public static class NumericExtensions
     {
@@ -12,6 +15,31 @@
         {
             ulong u = (ulong)l + long.MaxValue;
             return u;
+        }
+
+        public static IEnumerable<IEnumerable<TSource>> Batch<TSource>(
+            this IEnumerable<TSource> source, int size)
+        {
+            TSource[] bucket = null;
+            var count = 0;
+
+            foreach (var item in source)
+            {
+                if (bucket == null)
+                    bucket = new TSource[size];
+
+                bucket[count++] = item;
+                if (count != size)
+                    continue;
+
+                yield return bucket;
+
+                bucket = null;
+                count = 0;
+            }
+
+            if (bucket != null && count > 0)
+                yield return bucket.Take(count);
         }
     }
 }
