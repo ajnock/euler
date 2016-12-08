a25fc26086a6fba41edb3b98926519cc3d5b72a1
Adam Nock <adam.j.nock@gmail.com>
2016-12-08 18:42:21 -0500

    Better Sieve

--- Ulam/Sieve.js
+++ Ulam/Sieve.js
@@ -1,34 +1,30 @@
 function getNextPrime(p) {
-    var obj = db.map.find({
+    var obj = db.getSiblingDB("Ulam").map.find({
         $and: [
         { IsPrime: true },
         { _id: { $gt: p } }
         ]
     }).sort({ _id: 1 }).limit(1);
 
-    if (obj) {
-        return obj[0]._id;
-    } else {
-        return null;
-    }
+    return obj;
 }
 
 // start sieving at 3 because that's
 var prime = getNextPrime(1);
-var max = db.map.find().sort({ _id: -1 }).limit(1)._id;
-while (prime != null && prime <= max / 3) {
+var max = db.getSiblingDB("Ulam").map.find().sort({ _id: -1 }).limit(1)._id;
+while (prime != null && prime._id <= max) {
     print("Sieving " + prime);
 
-    db.map.updateMany({
+    db.getSiblingDB("Ulam").map.updateMany({
         $and: [
             { IsPrime: true },
-            { _id: { $gt: prime } },
-            { _id: { $mod: [prime, 0] } }
+            { _id: { $gt: prime._id } },
+            { _id: { $mod: [prime._id, 0] } }
         ]
     },
         { $set: { IsPrime: false } });
 
-    db.primes.insert({ _id: prime });
+    db.getSiblingDB("Ulam").primes.insert(prime);
 
-    prime = getNextPrime(prime);
+    prime = getNextPrime(prime._id);
 }
