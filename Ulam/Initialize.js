5851cb5177184f6424ece9eba9fdf6b4c881096a
Adam Nock <adam.j.nock@gmail.com>
2016-12-08 20:49:05 -0500

    Fixed creation script.

--- Ulam/Initialize.js
+++ Ulam/Initialize.js
@@ -2,6 +2,3 @@ db.map.drop();
 db.map.createIndex({ IsPrime: -1, Value: 1 }, { unique: true, background: true });
 db.map.createIndex({ Value: 1 }, { unique: true, background: true });
 //db.map.createIndex({ Location: 1 }, { unique: true, background: true });
-
-db.primes.drop();
-db.primes.createIndex({ Value: 1 }, { unique: true, background: true });
